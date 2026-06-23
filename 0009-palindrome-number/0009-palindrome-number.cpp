cac6756edc14c3856b64c8c91fd3345ea313a02c
mayank5370 <mayank.1290singh@gmail.com>
2026-06-23 22:42:54 +0530

    Time: 4 ms (31.78%), Space: 8.5 MB (92.21%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        if(x<0){
+            return false;
+        }
+        long long res = 0;
+        int temp = x;
+        while(temp!=0){
+            int ld = temp % 10;
+            res = res*10 + ld;
+            temp = temp/10;
+        }
+
+        return (res == x);
+    }
+};
