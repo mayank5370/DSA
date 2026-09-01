21a9d6ae49960eaec856b6d2badcaf65eab9f8ef
Mayank Singh <mayank.1290singh@gmail.com>
2026-09-01 22:13:55 +0530

    New Problem

---
+++ Logical Building/Check_Months.cpp
@@ -0,0 +1,67 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int Days(int month)
+{
+
+    switch (month)
+    {
+    case 1:
+        return 31;
+        break;
+
+    case 2:
+        return 28;
+        break;
+
+    case 3:
+        return 31;
+        break;
+
+    case 4:
+        return 30;
+        break;
+
+    case 5:
+        return 31;
+        break;
+
+    case 6:
+        return 30;
+        break;
+
+    case 7:
+        return 31;
+        break;
+
+    case 8:
+        return 31;
+        break;
+
+    case 9:
+        return 30;
+        break;
+
+    case 10:
+        return 31;
+        break;
+
+    case 11:
+        return 30;
+        break;
+
+    case 12:
+        return 31;
+        break;
+    }
+
+    return -1;
+}
+
+int main()
+{
+    int month;
+    cin >> month;
+
+    cout << Days(month) << endl;
+}
