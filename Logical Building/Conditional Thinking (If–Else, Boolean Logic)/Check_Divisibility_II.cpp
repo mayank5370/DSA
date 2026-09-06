a9d7b625fa180ecae27b09a40c7f20e4c3287253
Mayank Singh <mayank.1290singh@gmail.com>
2026-09-06 11:24:20 +0530

    New Problem

---
+++ Logical Building/Conditional Thinking (If–Else, Boolean Logic)/Check_Divisibility_II.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+string checkDivisible(int num){
+    if(num % 3 == 0 && num % 5 == 0){
+        return "FizzBuzz";
+    }
+    else if(num % 3 == 0){
+        return "Fizz";
+    }
+    else if(num % 5 == 0){
+        return "Buzz";
+    }
+    else {
+        return "Not divisible from both";
+    }
+}
+
+
+int main(){
+    int num;
+    cin >> num;
+
+    cout << checkDivisible(num) << endl;
+}
