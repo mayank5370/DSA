d98b4fd8f0395259785b51165bfa58edbb8a85ce
Mayank Singh <mayank.1290singh@gmail.com>
2026-09-06 23:01:00 +0530

    New Problem

---
+++ Logical Building/Conditional Thinking (If–Else, Boolean Logic)/Check_Tax_Eligible.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+string taxEligible(int age, int income){
+    if(age > 18 && income > 500000){
+        return "The persona is eligible for taxation";
+    }
+    else{
+        return "The person is not eligible for taxation";
+    }
+}
+
+
+int main(){
+    int age, income;
+    cin >> age;
+    cin >> income;
+
+    cout << taxEligible(age, income) << endl;
+}
+
