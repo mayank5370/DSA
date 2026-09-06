3b6eb355f67cd62f184c4628b5c3ab7737437726
Mayank Singh <mayank.1290singh@gmail.com>
2026-09-06 22:37:53 +0530

    New Problem

---
+++ Logical Building/Conditional Thinking (If–Else, Boolean Logic)/Check_Time.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+string dayTime(int hours, int mint){
+    if(hours > 0 && hours < 12){
+        return "AM";
+    }
+    else if(hours > 12 && hours <= 24){
+        return "PM";
+    }
+    else{
+        return "Invlid Time Period";
+    }
+}
+
+
+int main(){
+    int hour, min;
+    cin >> hour >> min;
+
+    cout << dayTime(hour, min) << endl;
+}
