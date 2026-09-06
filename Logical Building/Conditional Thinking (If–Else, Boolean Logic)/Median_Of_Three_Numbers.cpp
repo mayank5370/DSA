6134058bee5748437e3fcbcda5498af1222f9210
Mayank Singh <mayank.1290singh@gmail.com>
2026-09-06 21:37:20 +0530

    New Probblem

---
+++ Logical Building/Conditional Thinking (If–Else, Boolean Logic)/Median_Of_Three_Numbers.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int medianOf3(int num1, int num2, int num3){
+    if(num1 > num2 && num1 < num3 || num1 < num2 && num1 > num3){
+        return num1;
+    }
+    else if(num2 > num1 && num2 < num3 || num2 < num1 && num2 > num3){
+        return num2;
+    }
+    else{
+        return num3;
+    }
+}
+
+int main(){
+    int num1, num2, num3;
+    cin >> num1;
+    cin >> num2;
+    cin >> num3;
+
+    cout << medianOf3(num1, num2, num3) << endl;
+}
