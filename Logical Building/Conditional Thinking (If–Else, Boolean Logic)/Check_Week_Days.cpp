e9eb563df8077935366ad40d12aa67a1e571a069
Mayank Singh <mayank.1290singh@gmail.com>
2026-09-09 15:38:47 +0530

    New Problem

---
+++ Logical Building/Conditional Thinking (If–Else, Boolean Logic)/Check_Week_Days.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+string weekOrWeekend(int num){
+    if(num == 6 || num == 7){
+        return "Weekend Days";
+    }else{
+        return "Week Days";
+    }
+}
+
+int main(){
+    int days;
+    cin >> days;
+
+    cout << weekOrWeekend(days) << endl;
+}
