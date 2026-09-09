5f2ad9178eed4276249d8f0fc321c96c7aaa42d5
Mayank Singh <mayank.1290singh@gmail.com>
2026-09-09 21:01:56 +0530

    New Problem

---
+++ Logical Building/Conditional Thinking (If–Else, Boolean Logic)/Check_Valid_Calendar.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+string checkCalendar(int days, int month){
+    if(days == 31){
+        if(month == 1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 10 || month == 12){
+            return "Valid Month";
+        }
+        else{
+            return "Invalid Month";
+        }
+    }
+    else if(days == 30){
+        if(month == 4 || month == 6 || month == 9 || month == 10){
+            return "Valid Month";
+        }
+        else{
+            return "Invalid Month";
+        }
+    }
+    else if( days == 28 || days == 29){
+        if(month == 2){
+            return "Valid Month";
+        }
+        else{
+            return "Invalid Month";
+        }
+    }
+    else {
+        return "Invalid days and months";
+    }
+}
+
+int main(){
+    int days;
+    int month;
+
+    cin >> days;
+    cin >> month;
+
+    cout << checkCalendar(days, month) << endl;
+}
