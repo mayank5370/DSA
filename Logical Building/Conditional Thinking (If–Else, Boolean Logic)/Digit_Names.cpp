0dd42e49950ea48316f8c9cf63604e77baa8afe6
Mayank Singh <mayank.1290singh@gmail.com>
2026-09-09 15:21:37 +0530

    New Problem

---
+++ Logical Building/Conditional Thinking (If–Else, Boolean Logic)/Digit_Names.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+string digitToName(int num){
+    
+    switch(num){
+        case 1: 
+            return "Monday";
+            break;
+        
+        case 2:
+            return "Tuesday";
+            break;
+        
+        case 3: 
+            return "Wednesday";
+            break;
+
+        case 4:
+            return "Thursady";
+            break;
+        
+        case 5: 
+            return "Friday";
+            break;
+
+        case 6:
+            return "Saturday";
+            break;
+        
+        case 7:
+            return "Sunday";
+            break;
+    }
+}
+
+int main(){
+    int days;
+    cin >> days;
+
+    cout << digitToName(days) << endl;
+
+}
