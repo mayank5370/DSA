fac77494d9baf17091ee1f356d61f6f1cdd549cb
Mayank Singh <mayank.1290singh@gmail.com>
2026-09-09 20:16:06 +0530

    New Problem

---
+++ Logical Building/Conditional Thinking (If–Else, Boolean Logic)/Check_Points.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+string checkPoints(int x, int y){
+    if(x == 0 && y == 0){
+        return "The point lies on the origin";
+    }
+    else if(x == 0){
+        return "The ponit lies on the Y-axis";
+    }
+    else if(y == 0){
+        return "The point lies on X axis";
+    }
+    else {
+        return "The point doesn't lies on any axis";
+    }
+}
+
+int main(){
+    int x, y;
+    cin >> x >> y;
+
+    cout << checkPoints(x , y) << endl;
+}
