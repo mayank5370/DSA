48efc77d4ed6c47170ee0d38cd82d6a917af1f50
Mayank Singh <mayank.1290singh@gmail.com>
2026-09-12 21:29:42 +0530

    New Problem

---
+++ Logical Building/LOOPING & PATTERNS (ITERATION & FLOW)/Odd_Numbers_Sum.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int nSum(int num){
+    int sum = 0;
+    while(num >= 0){
+        if(num % 2 != 0){
+            sum += num;
+        }
+        
+        num --;
+    }
+
+    return sum;
+}
+
+int main(){
+    int num;
+    cin >> num;
+
+    cout << nSum(num) << endl;
+    
+    return 0;
+}
